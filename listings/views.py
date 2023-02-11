4c30a22505075857a691879cd007a93cc3ee9985
shahadatcs <shahadat.pust@gmail.com>
2023-02-11 20:21:51 +0600

    second commit

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
+from .models import Listing
+
 
 # Create your views here.
 def listings(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 3) # Show 3 contacts per page.
+    page_number = request.GET.get('page')
+    page_listings = paginator.get_page(page_number)
+    contex = {
+        'listings':page_listings
+    }
+    return render(request, 'listings/listings.html',contex)
 
-def listing(request):
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 def search(request):
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from listings.models import Listing
+from realtors.models import Realtor
 
 # Create your views here.
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context ={
+        'listings':listings
+    }
+    return render(request, 'pages/index.html', context)
+
 def about(request):
-    return render(request, 'pages/about.html')
+    # Get all realtors
+    realtors = Realtor.objects.order_by('-list_date')
+    # Get mvp realtors
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+    context ={
+        'realtors': realtors
+    }
+    return render(request, 'pages/about.html', context)
