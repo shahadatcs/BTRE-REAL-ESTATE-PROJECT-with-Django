ae8913afee12f0609339b5ad1e5a1396074d68b9
shahadatcs <shahadat.pust@gmail.com>
2023-02-16 22:01:45 +0600

    account app

--- btre/urls.py
+++ btre/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('listings/', include('listings.urls')),
    # path('realtors/', include('realtors.urls')),
    path('accounts/', include('accounts.urls')),
+   path('contacts/', include('contacts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
